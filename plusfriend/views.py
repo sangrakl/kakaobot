2ad68935a7985060a9503095b3a55c0387774e09
sangrakl <sangrakl@gmail.com>
2018-02-21 10:05:37 +0900

    커밋커밋

--- plusfriend/views.py
+++ plusfriend/views.py
@@ -11,3 +11,19 @@ def keyboard(request):
         'buttons': ['버튼1', '버튼2', '버튼3',]
         }
     )
+
+@csrf_exempt
+def message(request):
+
+    return JsonResponse(
+        {
+            'message': {
+                'text': '내가원하는 메세지'
+            }
+            'keyboard': {
+                'type': 'buttons',
+                'buttons': ['메시지벝믄1', '메시지벝믄2', '메시지벝믄3',]
+            }
+
+        }
+    )

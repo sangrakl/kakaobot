b0ca408528fe58d6898cda71ce3795626697b02a
sangrakl <sangrakl@gmail.com>
2018-02-21 11:27:46 +0900

    commit

--- plusfriend/functions.py
+++ plusfriend/functions.py
@@ -6,7 +6,7 @@ def melon_search(query):
         'jscallback': '_',
         'query': query,
     }
-    jsonp_string = requests.get('http://www.melon.com/search/keyword/index.json', params=params).text
+    jsonp_string = requests.get('http://www.melon.com/search/total/index.json', params=params).text
     json_string = jsonp_string.replace('_(', '').replace(');', '')
     meta = json.loads(json_string)
 
@@ -15,8 +15,10 @@ def melon_search(query):
         for song in meta['SONGCONTENTS']:
             messages.append('''[{ALBUMNAME}] {SONGNAME} by {ARTISTNAME}
 - http://www.melon.com/song/detail.htm?songId={SONGID}'''.format(**song))
+
         if messages:
             message = '\n'.join(messages)
         else:
             message = '검색어 "{}"에 대한 노래 검색결과가 없습니다.'.format(query)
+
         return message
